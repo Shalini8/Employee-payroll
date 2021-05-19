8dbd42271fb69d71ac123674dc86a2015cd23755
Shalini8 <shalu8mar@gmail.com>
2021-05-19 21:17:01 +0530

    Established Database connection

--- src/main/java/com/bridgelabz/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/EmployeePayrollService.java
@@ -51,7 +51,6 @@ public class EmployeePayrollService {
         if(ioService.equals(IOService.FILE_IO))
             new EmployeePayrollFileIOService().printData();
     }
-
     public long countEntries(IOService ioService){
         if(ioService.equals(IOService.FILE_IO))
            return new EmployeePayrollFileIOService().countEntries();
